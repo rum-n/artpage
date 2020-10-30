9e9f2c4f7d22331b1cff273ee7090d635baa6865
Rumen Manev <r.manev@blubito.com>
2020-10-30 11:19:49 +0200

    resolving warning

--- src/components/examples/Examples.js
+++ src/components/examples/Examples.js
@@ -3,6 +3,7 @@ import './styles.css';
 import violin from './../../assets/violin.jpeg';
 import sparrow from './../../assets/sparrow.png';
 import flower from './../../assets/flower.jpg';
+import { Link } from 'react-router-dom';
 
 const Examples = () => {
     return (
@@ -12,19 +13,19 @@ const Examples = () => {
                     <img src={sparrow} alt='Little Sparrow' />
                     <p className='art-title'>Little Sparrow</p>
                     <p className='painter'>Dony Boyadzhieva, 2018</p>
-                    <a className='art-link' href='/doni'>View gallery &#8594;</a>
+                    <Link><p className='art-link' href='#'>View gallery &#8594;</p></Link>
                 </div>
                 <div className='art-example'>
                     <img src={flower} alt='Protea Flower' />
                     <p className='art-title'>Protea Watercolor</p>
                     <p className='painter'>Violeta Boyadzhieva, 2018</p>
-                    <a className='art-link' href='#'>View gallery &#8594;</a>
+                    <Link><p className='art-link' href='#'>View gallery &#8594;</p></Link>
                 </div>
                 <div className='art-example'>
                     <img src={violin} alt='Violinist' />
                     <p className='art-title'>The Violinist</p>
                     <p className='painter'>Rumen Manev Sr., 2018</p>
-                    <a className='art-link' href='#'>View gallery &#8594;</a>
+                    <Link><p className='art-link' href='#'>View gallery &#8594;</p></Link>
                 </div>
             </div>
         </section>
