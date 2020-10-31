eba351648e2c23b23888b8a2a30df1037e06d455
Rumen Manev <r.manev@blubito.com>
2020-10-31 13:47:56 +0200

    artist page styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import './styles.css';
-// import logo from './../../assets/logo.png';
+import logo from './../../assets/logo.png';
 import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
+        <React.Fragment>
+            <hr/>
         <footer>
             <div className='footer-logo'>
-                {/* <img src={logo} alt='Podcast Chatterbox logo'/> */}
-                {/* <p>© 2020 Podcast Chatterbox. All rights reserved.</p> */}
+                <NavLink to='/'><img src={logo} alt='Podcast Chatterbox logo'/></NavLink>
+                {/* <p>© 2020 Artpage.</p> */}
             </div>
             <div>
-                <h4>Features</h4>
-                <p><NavLink className='main-blue' to='/hosts'>For Hosts</NavLink></p>
-                <p><NavLink className='main-blue' to='/guests'>For Guests</NavLink></p>
+                <h4>Twitter</h4>
+                {/* <p><NavLink className='main-blue' to='/hosts'>For Hosts</NavLink></p>
+                <p><NavLink className='main-blue' to='/guests'>For Guests</NavLink></p> */}
             </div>
             <div>
-                <h4>Contact</h4>
-                <p>for any bugs or feature requests</p>
-                <p>reach out via <a href='https://twitter.com/room_n'>Twitter</a></p>
+                <h4>Instagram</h4>
+                {/* <p>for any bugs or feature requests</p>
+                <p>reach out via <a href='https://twitter.com/room_n'>Twitter</a></p> */}
             </div>
             <div>
-                <h4>Credits</h4>
-                <p>Created by <a href='https://rmanev.com'>Rumen Manev</a></p>
-                <p>Illustrations by <a href='https://www.etsy.com/shop/Nordkins'>Nordkins</a></p>
+                <h4>Pinterest</h4>
             </div>
         </footer>
+        </React.Fragment>
     )
 }
 
--- src/pages/artist/artist.js
+++ src/pages/artist/artist.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import './styles.css';
 // import logo from './../../assets/logo.png';
+import { GrInstagram, GrTwitter } from "react-icons/gr";
+import { FaEtsy } from "react-icons/fa";
+import { SiSociety6 } from "react-icons/si";
 
 const Artist = () => {
     return (
         <div className='main'>
             <h1>Welcome to Doni's art gallery</h1>
+            <div className="artist-info">
+                <div className='artist-bio'>
+                    <h2>About Doni</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt maximus erat, ac elementum sem scelerisque a. Nam sit amet metus in ex sodales porttitor. Etiam facilisis pellentesque ex vel sollicitudin. In non urna lacus. Nunc sed gravida augue, non ullamcorper odio. Duis auctor fringilla dictum. Nam venenatis pulvinar libero, id rutrum lacus pharetra gravida. Curabitur bibendum accumsan pellentesque. Morbi odio dolor, semper non est nec, vehicula blandit nulla. Duis tempor in velit sodales porttitor. Nunc elit dolor, efficitur feugiat enim ac, egestas sodales neque. Praesent pulvinar ut purus quis tincidunt.</p>
+                </div>
+                <div className='external-links'>
+                    <h2>External links</h2>
+                    <div className="external-link-icons">
+                        <a href="https://www.instagram.com/nordkins/"><GrInstagram/></a>
+                        <a href="https://www.etsy.com/shop/Nordkins"><FaEtsy/></a>
+                        <a href="https://www.twitter.com/Nordkins"><GrTwitter/></a>
+                        <a href='https://society6.com/nordkins'><SiSociety6/></a>
+                    </div>
+                </div>
+                <div className='buy-info'>
+                    <h2>How to buy</h2>
+                    <ol>
+                        <li>Feel free to browse through my online shop at <a href="https://www.etsy.com/shop/Nordkins">Etsy</a> and order any of the prints you like there.</li>
+                        <li>Contact me directly and I'd be happy to take your order and ship it to you personally.</li>
+                    </ol>            
+                </div>
+            </div>
             <div className='artwork-wrapper'>
                 <div className='artpiece'></div>
                 <div className='artpiece'></div>
                 <div className='artpiece'></div>
                 <div className='artpiece'></div>
             </div>
-            <h2>About Doni</h2>
-            <h2>External links</h2>
-            <h2>How to buy</h2>
         </div>
     )
 }
